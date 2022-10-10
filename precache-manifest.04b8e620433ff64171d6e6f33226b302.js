480f2f23104b026a46d8c4645fa7f33755c3acf9
Sebastian Rojas <sebastianrojas812@gmail.com>
2022-10-10 13:43:25 -0400

    Updates

---
+++ precache-manifest.04b8e620433ff64171d6e6f33226b302.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1462b9f949679544fbae",
+    "url": "%7Bsebastianr812%7D.github.io/react-test/static/css/main.ce1802df.chunk.css"
+  },
+  {
+    "revision": "1462b9f949679544fbae",
+    "url": "%7Bsebastianr812%7D.github.io/react-test/static/js/main.71c4c073.chunk.js"
+  },
+  {
+    "revision": "aeb8a3f86efd8601b73f",
+    "url": "%7Bsebastianr812%7D.github.io/react-test/static/js/runtime~main.bb47b889.js"
+  },
+  {
+    "revision": "c96111af1cfa1f7ba540",
+    "url": "%7Bsebastianr812%7D.github.io/react-test/static/css/2.e1815414.chunk.css"
+  },
+  {
+    "revision": "c96111af1cfa1f7ba540",
+    "url": "%7Bsebastianr812%7D.github.io/react-test/static/js/2.345049e9.chunk.js"
+  },
+  {
+    "revision": "8fa34ba00032514f3d70",
+    "url": "%7Bsebastianr812%7D.github.io/react-test/static/js/3.490731cf.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "%7Bsebastianr812%7D.github.io/react-test/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a2b23b4f22883b405ca8824a3b42b627",
+    "url": "%7Bsebastianr812%7D.github.io/react-test/index.html"
+  }
+];
